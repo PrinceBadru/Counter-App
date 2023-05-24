3d770d32c7cdb414cc32adbb8633cb62ba9af002
Badru Ndawula <badrundawula24@gmail.com>
2023-05-24 09:56:50 +0300

    add save feature

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let countFunc= document.getElementById("count-btn");
 let clickNum = document.getElementById("innertext");
 let count = 0;
+let records = document.getElementById("notes")
+let savings = 0;
 
 function pressed(){
     count++ ;
     clickNum.textContent = count;
+}
+
+function keep(){
+ records.innerText = count;
+ count = 0;
+ clickNum.textContent = count ;
+
 }
